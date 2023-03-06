dcebd86ff7563f5a70abe95453959ae09674c3c2
Geezerkia <123498636+Geezerkia@users.noreply.github.com>
2023-03-06 21:49:45 +0200

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,13 +1,24 @@
 public class Car {
-    public static void start(){
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
         startElectricity();
         startCommand();
         startFuelSystem();
+        System.out.println("Engine is started!");
     }
-    private static void startElectricity(){
+
+    private static void startElectricity() {
+        System.out.println("Electricity is started.");
     }
-    private static void startCommand(){
+
+    private static void startCommand() {
+        System.out.println("Command is started.");
     }
-    private static void startFuelSystem(){
+
+    private static void startFuelSystem() {
+        System.out.println("Fuel System is started.");
     }
 }
